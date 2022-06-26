cf8939e0ab774c11d66abfe636d257373c988e01
= <=>
2022-06-27 00:04:23 +0300

    add config & react

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React, { FC } from "react";
+
+const App: FC = () => {
+  return (
+    <div>
+      The same card game
+    </div>
+  )
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+
+module.exports = {
+  mode: 'development',
+  entry: ['./src/index.tsx'],
+  resolve: {
+    extensions: [".tsx", ".js", ".ts"],
+  },
+  output: {
+    filename: '[name].build.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['babel/preset-env', 'babel/preset-react'],
+          },
+        }
+      },
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+
+    ]
+  },
+  devServer: {
+    port: 4000,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+  ]
+}
